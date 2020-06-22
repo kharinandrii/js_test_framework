ee35f4f0c05e8e883f1bb3b347b814d5af63bc4f
AndyQa <andrej.kharin@gmail.com>
2020-06-22 16:48:02 +0100

    first commit

---
+++ specs/first.spec.js
@@ -0,0 +1,7 @@
+
+const expect = require('chai').expect
+describe('First suit', function (){
+    it('first test', async function(){
+        await browser.get('https://teamring.tcl.ukrtech.info/')
+    })
+})
